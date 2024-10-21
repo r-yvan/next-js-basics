e857e874b9580993b88d559bd11d3c52126a0202
r-yvan <yvankiliye.rubuto@gmail.com>
2024-10-21 10:21:24 +0200

    Commit

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,9 +1,27 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+
 export const GET = async (request: NextRequest) => {
-  const users = [
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Jane Smith" },
-    { id: 3, name: "Bob Johnson" },
-  ]
-  return NextResponse.json(users)
-}
+  const users = await prisma.users.findMany();
+  return NextResponse.json(users);
+};
+
+export const POST = async (request: NextRequest) => {
+  const body = await request.json();
+  const usersFromDb = await prisma.users.findMany();
+  const userFromDb = await usersFromDb.find(
+    (user) => user.email === body.email
+  );
+  if (userFromDb)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+
+  const newUser = await prisma.users.create({
+    data: {
+      username: body.name,
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  return NextResponse.json(newUser);
+};
