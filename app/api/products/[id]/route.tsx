3e45350bcdc985b0ba63ffb9a3d902cc15cb6928
r-yvan <yvankiliye.rubuto@gmail.com>
2024-11-03 11:03:11 +0200

    Commit

--- app/api/products/[id]/route.tsx
+++ app/api/products/[id]/route.tsx
@@ -1,7 +1,9 @@
 import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import { PrismaClient } from "@prisma/client/extension";
 
+const prisma = new PrismaClient();
 export const GET = (
   request: NextRequest,
   { params }: { params: { id: number } }
@@ -39,6 +41,12 @@ export const POST = async (
       { error: "The Product cannot be created!!" },
       { status: 404 }
     );
+  const newProd = await prisma.products.create({
+    data: {
+      name: body.name,
+      price: body.price,
+    }
+  });
   return NextResponse.json({
     body,
     message: "The Product created successfully!!",
@@ -55,5 +63,8 @@ export const PUT = async (
   if (!validationResult.success)
     return NextResponse.json(validationResult.error.errors, { status: 400 });
 
-  return NextResponse.json({...body, message: "Successfully updated the object!!"})
+  return NextResponse.json({
+    ...body,
+    message: "Successfully updated the object!!",
+  });
 };
--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,9 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from "@/prisma/client";
 
 export const GET = async (request: NextRequest) => {
   const prods = await prisma.products.findMany();
   return NextResponse.json(prods);
 };
+ 
